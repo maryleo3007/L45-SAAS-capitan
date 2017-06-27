b10864c981f1dc385f9a59ddceddbe74a0e5808c
Janine Vega <osmary.vega@gmail.com>
2017-06-27 18:08:01 -0500

    update json2

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
 
 $( _ => {
 
-  getJSON('laboratoria.cuadra.co:9339/api/v1/students/', (err, json) => {
+  getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/', (err, json) => {
     if (err) { return alert(err.message);}
 
     const studentList = $('#studentList');
