7a304ab54b31799273edc3451d35f39ab443236b
Janine Vega <osmary.vega@gmail.com>
2017-06-27 13:26:34 -0500

    l45 maquetado ok

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$( _ => {
+
+  getJSON('http://laboratoria.cuadra.co:9339/api/v1/students/', (err, json) => {
+
+    if (err) { return alert(err.message);}
+
+    console.log(json);
+    const studentList = $('#studentList');
+    json.forEach((e)=>{
+        studentList.append('<li>'+e.name+'<input type="checkbox"></li>');
+    });
+
+  });
+
+});
